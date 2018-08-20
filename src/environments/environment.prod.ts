50ba26deb69c0c812d93c288d9dd7a68e0d40764
karmak40 <sikomat@mail.ru>
2018-08-20 20:55:05 +0200

    add firebase api keys, material design , ceate few modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB68U01uqjKF9r1EEOl00L8jmFdQKklajI",
+    authDomain: "ng-fire-2e216.firebaseapp.com",
+    databaseURL: "https://ng-fire-2e216.firebaseio.com",
+    projectId: "ng-fire-2e216",
+    storageBucket: "ng-fire-2e216.appspot.com",
+    messagingSenderId: "101788103425"
+  } 
 };
